bff9c20d9294ba3633b207e9120e803b988e5635
Ravi Sharma <sravit@sysml.ee.ucla.edu>
2024-06-13 09:58:42 -0700

    (Buggy) simplified implementation.

--- cim_utils.py
+++ cim_utils.py
@@ -2,9 +2,7 @@ from torch import Tensor
 import torch
 from typing import Optional, List, Tuple, Union
 from torch.nn import functional as F
-import torch.ao.quantization.observer as observer
 from torch.nn.modules.utils import _pair
-from torch.ao.quantization.observer import MinMaxObserver, HistogramObserver
 import numpy as np
 import math
 import ast
@@ -14,83 +12,17 @@ from load_config import load_config
 
 args = load_config()
 
-clamping_range_start = args.clamping_range_start # May be modified by main.py
-clamping_range_mode = args.clamping_range_mode
 conv_mode = "float"
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-ObserverCls = MinMaxObserver #HistogramObserver
+clamping_range_start = args.clamping_range_start
+clamping_range_mode = args.clamping_range_mode
 clamping = False
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 s, zp = 0.03, 0
-
-class AverageMeter(object):
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
-def get_quant_values_observer(input, observer, bitwidth):
-    observer = None
-    if observer is not None:
-        min_, max_ = observer.quant_min, observer.quant_max
-        s, zp = observer.calculate_qparams()
-    else:
-        min_, max_ = (-2**(bitwidth-1)), (2**(bitwidth-1))-1
-        s, zp = torch.Tensor([s]).to(device), torch.Tensor([0.0]).to(device)
-    return s, zp, min_, max_
-
-def get_quant_values_no_observer(input, bitwidth):
-    if args.quant_sign == "asymmetric":
-        alpha, beta = torch.max(input), torch.min(input)
-        s = (alpha - beta) / (2**bitwidth-1)
-        zp = -1*torch.round(beta / s)
-        min_ = 0
-        max_ = (2**bitwidth)-1
-    elif conv_mode == "symmetric":
-        s = torch.max(torch.abs(input)) / (2**bitwidth - 1)
-        zp = 0
-        min_ = -2**(bitwidth-1)
-        max_ = 2**(bitwidth-1)-1
-    return s, zp, min_, max_
-
-class Quantize(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, x, s, zp, min, max):
-        ctx.s = s
-        x = (x - zp) / s
-        x = torch.round(x)
-        x = torch.clamp(x, min, max)
-        return x
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        return grad_output/ctx.s, None, None, None, None
-
-class Dequantize(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, x, s, zp, min, max):
-        ctx.s = s
-        x = (x * s) + zp
-        return x
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        return grad_output*ctx.s, None, None, None, None
 
 class FakeQuantize(torch.autograd.Function):
     @staticmethod
     def forward(ctx, x, s, zp, min, max):
-        return x
         x = (x - zp) / s
         x = torch.round(x)
         x = torch.clamp(x, min, max)
@@ -114,7 +46,6 @@ class FakeADC(torch.autograd.Function):
             min, max = -(2**(bitwidth-1)), (2**(bitwidth-1))-1
             y = torch.round(torch.clamp(y, min, max))
             y = y * (2**args.clamping_range_start)
-        if clamping:
             y = y * (x_s * w_s)
         return y
 
@@ -137,24 +68,11 @@ class ADC_Conv2d(torch.nn.modules.Conv2d):
     def __init__(self, *kargs, **kwargs):
         super().__init__(*kargs, **kwargs)
 
-        self.observers_initialized = False
-        self.input_observer = None
-        self.truncate_input_observer = None
-
-        self.truncate_input_mean_avg_meter = AverageMeter()
-        self.truncate_input_var_avg_meter = AverageMeter()
-
-        self.gain = torch.Tensor([1.]).to(device) if clamping_range_mode=="per_model" else torch.nn.parameter.Parameter(torch.Tensor([1.]))
         self.bn = MyBatchNorm2d(kargs[1])
         self.relu = True
 
         self.register_buffer('weight_org', self.weight.data.clone())
        
-    def initialize_observers(self):
-        self.observers_initialized = True
-        self.input_observer = ObserverCls(quant_min=-2**(args.act_bit-1), quant_max=2**(args.act_bit-1)-1, qscheme=torch.per_tensor_symmetric, dtype=torch.qint32).to(device)
-        self.truncate_input_observer = ObserverCls(quant_min=-2**(args.act_bit-1), quant_max=2**(args.act_bit-1)-1, qscheme=torch.per_tensor_symmetric, dtype=torch.qint32).to(device)
-    
     def fuse_batchnorm(self):
         weight_mul = self.bn.weight/torch.sqrt(self.bn.running_var+1e-5)
         weight_mul = torch.unsqueeze(torch.unsqueeze(torch.unsqueeze(weight_mul, 1), 1), 1)
@@ -167,7 +85,6 @@ class ADC_Conv2d(torch.nn.modules.Conv2d):
         self.bn.deactivated = True
         
     def calculate_qparams(self):
-        # use self.input_observer, args.act_bit, args.weight_bit, args.quant_sign
         self.x_s, self.x_zp = s, zp
         self.x_min, self.x_max = -2**args.act_bit, 2**args.act_bit-1
         self.w_s, self.w_zp = s, zp
@@ -178,17 +95,13 @@ class ADC_Conv2d(torch.nn.modules.Conv2d):
         global truncate
         input_, weight_, bias_ = input, weight, bias
 
-        if conv_mode == "observe":
-            if not self.observers_initialized:
-                self.initialize_observers() 
-            self.input_observer.forward(input_)
-
-        if conv_mode == "observe" or conv_mode == "quantize":
+        if conv_mode == "quantize" or conv_mode == "adc_quantize":
             input_.data = FakeQuantize.apply(input_.data, self.x_s, self.x_zp, self.x_min, self.x_max)
             weight_.data = FakeQuantize.apply(weight_.data, self.w_s, self.w_zp, self.w_min, self.w_max)
-            bias_.data = FakeQuantize.apply(bias_.data, self.w_s, self.w_zp, self.w_min, self.w_max)
+            if bias is not None:
+                bias_.data = FakeQuantize.apply(bias_.data, self.w_s, self.w_zp, self.w_min, self.w_max)
 
-            if args.cim_signed_type == "unipolar":
+            if conv_mode == "quantize" or args.cim_signed_type == "unipolar":
                 loops = [(input_, weight_, 1.)]
             elif args.cim_signed_type == "differential_pair":
                 loops = [(input_, torch.relu(weight_), 1.), (input_, torch.relu(-weight_), -1.)]
@@ -209,13 +122,9 @@ class ADC_Conv2d(torch.nn.modules.Conv2d):
                         else:
                             output_slice = F.conv2d(input_slice, weight_slice, bias, self.stride,
                                             self.padding, self.dilation, 1) #self.groups)
-                        if conv_mode == "observe":
-                            self.truncate_input_observer.forward(output_slice)
-                            if truncate_observe_mode == "mean":
-                                self.truncate_input_mean_avg_meter.update(float(torch.mean(output_slice)))
-                            elif truncate_observe_mode == "var":
-                                self.truncate_input_var_avg_meter.update((float(torch.mean(output_slice))-self.truncate_input_mean_avg_meter.avg)**2)
-                        output_slice = FakeADC.apply(output_slice, self.gain, self.x_s, self.w_s, args.clamping_range_start, args.act_bit)
+                        if conv_mode == "adc_quantize":
+                            gain_val = self.gain if self.gain_enabled else 1
+                            output_slice = FakeADC.apply(output_slice, gain_val, self.x_s, self.w_s, args.clamping_range_start, args.act_bit)
                         output += output_slice
                 else: # Depthwise convolution (assuming group_size < 128)
                     if self.padding_mode != 'zeros':
@@ -225,9 +134,9 @@ class ADC_Conv2d(torch.nn.modules.Conv2d):
                     else:
                         output = F.conv2d(input_, weight_, bias, self.stride,
                             self.padding, self.dilation, self.groups)
-                    if conv_mode == "observe":
-                        self.truncate_input_observer.forward(output)
-                    output = FakeADC.apply(output, self.gain, self.x_s, self.w_s, args.clamping_range_start, args.act_bit)
+                    if conv_mode == "adc_quantize":
+                        gain_val = self.gain if self.gain_enabled else 1
+                        output = FakeADC.apply(output, gain_val, self.x_s, self.w_s, args.clamping_range_start, args.act_bit)
                 
                 output = output * sign
                 total_output += output
--- main.py
+++ main.py
@@ -18,16 +18,14 @@ from load_config import load_config
 args = load_config()
 
 torch.autograd.set_detect_anomaly(True)
-quantize_mode=False
-
 logger = logger.Logger(args.log_dir)
-
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
-n_epochs = args.train_epochs
 
 FLOAT_CKPT_PATH = "./" + "_".join([args.net_name, "float"]) + ".pth"
-QUANT_CKPT_PATH = "./" + "_".join([args.net_name, "quant", args.clamping_range_mode]) + ".pth"
+NONFUSED_QUANT_CKPT_PATH = "./" + "_".join([args.net_name, "quant", args.clamping_range_mode, "nonfused_bn"]) + ".pth"
+QUANT_CKPT_PATH = "./" + "_".join([args.net_name, "quant", args.clamping_range_mode, "fused_bn" if args.fuse_bn else "nonfused_bn"]) + ".pth"
+ADC_QUANT_CKPT_PATH = "./" + "_".join([args.net_name, "adc_quant", args.clamping_range_mode, "fused_bn" if args.fuse_bn else "nonfused_bn", args.cim_signed_type]) + ".pth"
 CKPT_PATH = FLOAT_CKPT_PATH
 
 transform_train = transforms.Compose([
@@ -91,12 +89,12 @@ def train(epoch):
         outputs = net(inputs)
         loss = criterion(outputs, targets)
         loss.backward()
-        if cim_utils.conv_mode=="quantize":
+        if cim_utils.conv_mode in ["quantize", "adc_quantize"]:
             for p in net.modules():
                 if isinstance(p, nn.Conv2d):
                     p.weight.data.copy_(p.weight_org)
         optimizer.step()
-        if cim_utils.conv_mode=="quantize":
+        if cim_utils.conv_mode in ["quantize", "adc_quantize"]:
             for p in net.modules():
                 if isinstance(p, nn.Conv2d):
                     p.weight_org.data.copy_(p.weight.data)
@@ -119,7 +117,7 @@ def test(epoch):
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(testloader):
             inputs, targets = inputs.to(device), targets.to(device)
-            if cim_utils.conv_mode=="quantize":
+            if cim_utils.conv_mode in ["quantize", "adc_quantize"]:
                 for p in net.modules():
                     if hasattr(p, "weight_org"):
                         p.weight.data.copy_(p.weight_org)
@@ -140,6 +138,7 @@ def test(epoch):
 def run_training():
     best_train_acc = 0
     best_test_acc = 0
+    n_epochs = args.float_train_epochs if cim_utils.conv_mode=="float" else args.quant_train_epochs
     for epoch in range(0, n_epochs):
         train_acc = train(epoch)
         test_acc = test(epoch)
@@ -158,6 +157,15 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 
+# Initialize gain value in ADC_Conv2d layers
+gain = torch.nn.parameter.Parameter(torch.Tensor([1.]).to(device)) if args.clamping_range_mode == "per_model" else None
+for p in net.modules():
+    if isinstance(p, cim_utils.ADC_Conv2d):
+        if args.clamping_range_mode == "per_layer":
+            gain = torch.nn.parameter.Parameter(torch.Tensor([1.]).to(device))
+        p.gain = gain
+        p.gain_enabled = True
+
 # Load & pretrain floating point network
 cim_utils.conv_mode = "float"
 if os.path.exists(FLOAT_CKPT_PATH):
@@ -172,31 +180,10 @@ else:
 test_accuracy = test(0)
 logger.log("FLOAT TESTING ACCURACY IS: " + str(test_accuracy))
 
-# TODO: Uncomment when Pytorch observers implemented
-"""
-# Observe in 2 passes- one for mean, one for var
-logger.log("OBSERVING DATA")
-cim_utils.conv_mode = "observe"
-cim_utils.truncate_observe_mode = "mean"
-observe_data()
-cim_utils.truncate_observe_mode = "var"
-observe_data()
-"""
-
 # Calculate quantization parameters
 for p in net.modules():
     if isinstance(p, cim_utils.ADC_Conv2d):
         p.calculate_qparams()
-
-# Fuse batchnorm
-if args.fuse_bn:
-    for p in net.modules():
-        if isinstance(p, cim_utils.ADC_Conv2d):
-            p.fuse_batchnorm()
-
-    # Test fused-bn floating point network
-    test_accuracy = test(0)
-    logger.log("FUSED BN TESTING ACCURACY IS: " + str(test_accuracy))
 
 # Load pretrained quantized network if it exists
 cim_utils.conv_mode = "quantize"
@@ -204,22 +191,36 @@ if os.path.exists(QUANT_CKPT_PATH):
     logger.log("LOADING PRETRAINED QUANTIZED NETWORK")
     net.load_state_dict(torch.load(QUANT_CKPT_PATH), strict=False)
 
+# Fuse batchnorm
+if args.fuse_bn:
+    if not os.path.exists(NONFUSED_QUANT_CKPT_PATH):
+        raise Exception
+    net.load_state_dict(torch.load(NONFUSED_QUANT_CKPT_PATH), strict=False)
+
+    print("Stage 1", test(0))
+
+    for p in net.modules():
+        if isinstance(p, cim_utils.ADC_Conv2d):
+            p.fuse_batchnorm()
+
+    print("Stage 2", test(0))
+
 # Train quantized network
-cim_utils.clamping = False
-if not os.path.exists(QUANT_CKPT_PATH) or args.perform_qat:
+CKPT_PATH = QUANT_CKPT_PATH
+if not os.path.exists(QUANT_CKPT_PATH):
+    cim_utils.conv_mode = "adc_quantize"
+    cim_utils.clamping = False
     logger.log("TRAINING QUANTIZED NETWORK")
-    CKPT_PATH = QUANT_CKPT_PATH
     best_train_acc, best_test_acc = run_training()
     logger.log("BEST QUANTIZED TESTING ACCURACY IS: " + str(best_test_acc))
 
 # Test quantized network without clamping
+cim_utils.conv_mode = "adc_quantize"
 cim_utils.clamping = False
-#net.load_state_dict(torch.load(QUANT_CKPT_PATH), strict=False)
 test_acc = test(0)
 logger.log("BEST QUANTIZED TESTING ACCURACY (No clamping) IS: " + str(test_acc))
 
 # Test quantized network with clamping
 cim_utils.clamping = True
-#net.load_state_dict(torch.load(QUANT_CKPT_PATH), strict=False)
 test_acc = test(0)
 logger.log("BEST QUANTIZED TESTING ACCURACY (Clamping) IS: " + str(test_acc))
